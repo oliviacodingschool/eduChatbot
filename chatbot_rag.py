87493624ac79c9faab702f2066005f5b27b65388
oliviacodingschool <115437017+oliviacodingschool@users.noreply.github.com>
2025-05-13 14:48:56 +0900

    Update chatbot_rag.py

--- chatbot_rag.py
+++ chatbot_rag.py
@@ -39,19 +39,20 @@ if st.button("질문하기") and user_input:
 
     candidate_answers = [sentences[i] for i in I[0]]
     st.markdown(f"**챗봇:** {candidate_answers}")
-    keywords = ["인구", "사람"]
-    matched_answer=None
-    for answer in candidate_answers:
-        if any(kw in answer for kw in keywords):
-            matched_answer = answer
-            break
-
-    # 키워드로 매칭된 게 없다면 첫 번째 후보 사용
-    if not matched_answer:
-        matched_answer = candidate_answers[0]
     
-    st.markdown(matched_answer)
-    if best_score > 500.0:
-        st.markdown(f"**챗봇:** 질문이 잘 이해되지 않습니다. 다른 방식으로 질문해주세요. 6Quiz를 활용해봐요!")
-    else:
-        st.markdown(f"**챗봇:** {matched_answer}")
+keywords = ["인구", "사람"]
+matched_answer=None
+for answer in candidate_answers:
+    if any(kw in answer for kw in keywords):
+        matched_answer = answer
+        break
+
+# 키워드로 매칭된 게 없다면 첫 번째 후보 사용
+if not matched_answer:
+    matched_answer = candidate_answers[0]
+
+st.markdown(matched_answer)
+if best_score > 500.0:
+    st.markdown(f"**챗봇:** 질문이 잘 이해되지 않습니다. 다른 방식으로 질문해주세요. 6Quiz를 활용해봐요!")
+else:
+    st.markdown(f"**챗봇:** {matched_answer}")
